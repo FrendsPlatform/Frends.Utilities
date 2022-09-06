7c6392d9592511f4d65015d95da322136ef66d50
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-09-06 10:30:29 +0300

    init

---
+++ Frends.Utilities.Sleep/Frends.Utilities.Sleep/Definitions/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace Frends.Utilities.Sleep.Definitions;
+
+/// <summary>
+/// Result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Sleep complete.
+    /// </summary>
+    /// <example>true</example>
+    public bool SleepResult { get; private set; }
+
+    internal Result(bool sleepResult)
+    {
+        SleepResult = sleepResult;
+    }
+}
