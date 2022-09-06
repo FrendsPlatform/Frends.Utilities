eb272e9017eedefad0cdaf7b90211d59e53d5a7a
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-09-06 10:48:47 +0300

    init

---
+++ Frends.Utilities.ThrowException/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [1.0.0] - 2022-09-06
+### Added
+- Initial implementation
---
+++ Frends.Utilities.ThrowException/Frends.Utilities.ThrowException/Definitions/Input.cs
@@ -0,0 +1,13 @@
+﻿namespace Frends.Utilities.ThrowException.Definitions;
+
+/// <summary>
+/// Input parameter.
+/// </summary>
+public class Input
+{
+    /// <summary>
+    /// Exception message.
+    /// </summary>
+    /// <example>Example Text.</example>
+    public string InputString { get; set; }
+}
